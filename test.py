7705c8fec89ce18dd6c9dc0f5c2242f1b4b83e1a
romesalvid <23-70207@g.batstate-u.edu.ph>
2026-05-02 17:00:50 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import time
 
 options = Options()
-options.add_argument("--headless=new")
+options.add_argument("--headless=new")      # new headless mode
 options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1920,1080")
 
-service = Service("/usr/bin/chromedriver")
 driver = webdriver.Chrome(service=service, options=options)
-driver.get("http://localhost")
-time.sleep(2)
-assert "Hello CI/CD World" in driver.page_source
-print("TEST PASSED")
-driver.quit()
